ace5f27b2a78e013877ae0465954eb0a9b5b349a
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-12-11 11:48:29 +0000

    Add task 0

--- utils/redis.js
+++ utils/redis.js
@@ -1,1 +1,53 @@
-#!/usr/bin/node
+import redis from 'redis';
+
+class CacheHandler {
+  constructor() {
+    this.connection = redis.createClient();
+    this.connection.on('error', (err) => {
+      console.error(err);
+    });
+  }
+
+  isOperational() {
+    return this.connection.connected;
+  }
+
+  async fetch(keyName) {
+    return new Promise((resolve, reject) => {
+      this.connection.get(keyName, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
+
+  async store(keyName, valueData, ttl) {
+    return new Promise((resolve, reject) => {
+      this.connection.setex(keyName, ttl, valueData, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  async remove(keyName) {
+    return new Promise((resolve, _reject) => {
+      this.connection.del(keyName, (err) => {
+        if (err) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  }
+}
+
+const cacheHandler = new CacheHandler();
+export default cacheHandler;
