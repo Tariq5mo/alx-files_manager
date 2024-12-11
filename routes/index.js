94282f7de928c7369c54570ad3522d59423d6185
Tariq5mo <tariqomer756@gmail.com>
2024-12-11 15:04:55 +0200

    Fix task 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const express = require('express');
-
-const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+
+const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
