30687ca948def2c71142786cca2624bd220322b1
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2024-12-11 20:58:40 +0000

    Fix task 2

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-#!/usr/bin/node
 const express = require('express');
 const routes = require('./routes/index');
 
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
