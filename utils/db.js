388c73a6fb0860e3af028950c8d79da5ad5366ec
Tariq5mo <tariqomer756@gmail.com>
2024-12-11 13:56:56 +0200

    Fix task 0

--- utils/db.js
+++ utils/db.js
@@ -1,1 +1,34 @@
 #!/usr/bin/node
+import { MongoClient } from 'mongodb';
+
+class DatabaseClient {
+  constructor() {
+    const dbHost = process.env.DB_HOST || 'localhost';
+    const dbPort = process.env.DB_PORT || 27017;
+    const dbName = process.env.DB_DATABASE || 'files_manager';
+    const connectionString = `mongodb://${dbHost}:${dbPort}`;
+
+    MongoClient.connect(connectionString, { useUnifiedTopology: true }, (error, client) => {
+      if (error) {
+        this.database = null;
+      } else {
+        this.database = client.db(dbName);
+      }
+    });
+  }
+
+  isConnected() {
+    return !!this.database;
+  }
+
+  async countUsers() {
+    return this.database.collection('users').countDocuments();
+  }
+
+  async countFiles() {
+    return this.database.collection('files').countDocuments();
+  }
+}
+
+export default new DatabaseClient();
+
--- utils/redis.js
+++ utils/redis.js
@@ -1,53 +1,31 @@
-import redis from 'redis';
+import { createClient } from 'redis';
+import { promisify } from 'util';
 
-class CacheHandler {
+class RedisClient {
   constructor() {
-    this.connection = redis.createClient();
-    this.connection.on('error', (err) => {
-      console.error(err);
-    });
+    this.client = createClient();
+    this.client.on('error', (err) => console.error('Redis client error:', err));
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
   }
 
-  isOperational() {
-    return this.connection.connected;
+  isAlive() {
+    return this.client.connected;
   }
 
-  async fetch(keyName) {
-    return new Promise((resolve, reject) => {
-      this.connection.get(keyName, (err, data) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      });
-    });
+  async get(key) {
+    return await this.getAsync(key);
   }
 
-  async store(keyName, valueData, ttl) {
-    return new Promise((resolve, reject) => {
-      this.connection.setex(keyName, ttl, valueData, (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-    });
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
   }
 
-  async remove(keyName) {
-    return new Promise((resolve, _reject) => {
-      this.connection.del(keyName, (err) => {
-        if (err) {
-          resolve(false);
-        } else {
-          resolve(true);
-        }
-      });
-    });
+  async del(key) {
+    await this.delAsync(key);
   }
 }
 
-const cacheHandler = new CacheHandler();
-export default cacheHandler;
+const redisClient = new RedisClient();
+export default redisClient;
