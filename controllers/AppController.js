807a5c26a9e052a8c6d8242d114d2fbe3f966f53
Tariq5mo <tariqomer756@gmail.com>
2024-12-11 14:29:09 +0200

    Add task 2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+  static async getStatus(req, res) {
+    res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  }
+
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    res.status(200).json({ users: usersCount, files: filesCount });
+  }
+}
+
+module.exports = AppController;
--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const express = require('express');
+
+const router = express.Router();
+const AppController = require('../controllers/AppController');
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+
+module.exports = router;
