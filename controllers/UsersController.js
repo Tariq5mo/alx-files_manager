f5414b181ca64b861291c51a7667e7013b572b31
Tariq5mo <tariqomer756@gmail.com>
2024-12-11 14:35:00 +0200

    Add task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,1 +1,29 @@
 #!/usr/bin/node
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const userExists = await dbClient.usersCollection.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+    const newUser = await dbClient.usersCollection.insertOne({ email, password: hashedPassword });
+
+    return res.status(201).json({ id: newUser.insertedId, email });
+  }
+}
+
+module.exports = UsersController;
