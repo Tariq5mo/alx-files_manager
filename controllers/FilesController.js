d930190338f3712763ac88f060b1b19b7397620a
Tariq5mo <tariqomer756@gmail.com>
2024-12-11 15:47:19 +0200

    Add task 6

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -49,6 +49,42 @@ class FilesController {
     const result = await dbClient.db.collection('files').insertOne(fileDocument);
     return res.status(201).json(result.ops[0]);
   }
+
+  static async getShow(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+    const fileId = req.params.id;
+    const fileDocument = await dbClient.db.collection('files').findOne({ _id: fileId, userId });
+    if (!fileDocument) return res.status(404).json({ error: 'Not found' });
+
+    return res.status(200).json(fileDocument);
+  }
+
+  static async getIndex(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+
+    const parentId = req.query.parentId || 0;
+    const page = parseInt(req.query.page, 10) || 0;
+    const pageSize = 20;
+
+    const files = await dbClient.db.collection('files')
+      .aggregate([
+        { $match: { userId, parentId } },
+        { $skip: page * pageSize },
+        { $limit: pageSize },
+      ])
+      .toArray();
+
+    return res.status(200).json(files);
+  }
 }
 
 export default FilesController;
