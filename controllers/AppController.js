9c879fd9572751472a241d6d2e26fa3050aa4986
Ahmed Bentabet <61090548+ahmedbentabet@users.noreply.github.com>
2025-01-17 10:34:38 +0000

    Fix task 2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,15 +4,15 @@ const dbClient = require('../utils/db');
 
 class AppController {
   static getStatus(req, res) {
-    const rL = redisClient.isAlive();
-    const dbL = dbClient.isAlive();
-    res.status(200).json({ redis: rL, db: dbL });
+    const redis = redisClient.isAlive();
+    const db = dbClient.isAlive();
+    res.status(200).json({ redis, db });
   }
 
   static async getStats(req, res) {
-    const totalUsers = await dbClient.nbUsers();
-    const totalFiles = await dbClient.nbFiles();
-    res.status(200).json({ users: totalUsers, files: totalFiles });
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).json({ users, files });
   }
 }
 
